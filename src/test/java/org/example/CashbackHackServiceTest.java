d2de04036ecc5c96f3c3efd8cfac5b80a9abfc2b
ЕленаПрекрасная <timoninae079@gmail.com>
2022-10-06 21:13:19 +0300

    'test'

---
+++ src/test/java/org/example/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+public class CashbackHackServiceTest {
+   import static org.testng.Assert.assertEquals;
+
+    public class CashbackHackServiceTest {
+
+        @Test
+        public void shouldRemainIfUnderBoundary() {
+            CashbackHackService service = new CashbackHackService();
+            int expected = 100;
+            int amount = 900;
+
+            int actual = service.remain(amount);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void shouldRemainIfOverBoundary() {
+            CashbackHackService service = new CashbackHackService();
+            int expected = 100;
+            int amount = 1900;
+
+            int actual = service.remain(amount);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void shouldNotRemainIfBoundary() {
+            CashbackHackService service = new CashbackHackService();
+            int expected = 0;
+            int amount = 1000;
+
+            int actual = service.remain(amount);
+            assertEquals(expected, actual);
+        }
